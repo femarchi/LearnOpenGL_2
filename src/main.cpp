43b9966ebbd445235835eef3c72b21c69a15bc5a
Felipe Marchi <fepmarchi@gmail.com>
2021-08-29 15:42:33 -0300

    Draw multiple cubes in the same viewport

--- src/main.cpp
+++ src/main.cpp
@@ -247,6 +247,33 @@ void setShaderProjectionMatrix(unsigned int shaderId)
 	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 }
 
+void drawMultipleCubes(unsigned int shaderId)
+{
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
+	for (unsigned int i = 0; i < 10; i++)
+	{
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, cubePositions[i]);
+		float angle = 20.0f * i;
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		unsigned int modelLoc = glGetUniformLocation(shaderId, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+}
+
 int main()
 {
 	std::cout << "Hello Coordinates" << std::endl;
@@ -303,7 +330,7 @@ int main()
 		setShaderViewMatrix(ourShader.ID);
 		setShaderProjectionMatrix(ourShader.ID);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		drawMultipleCubes(ourShader.ID);
 
 		glfwSwapBuffers(window); // show buffered pixels
 		glfwPollEvents(); // check keyboard, mouse and other events
